abf4ddfe1ab1ca54c6dd02465726c2b22615a63e
YVakhnyak <namejfk@gmail.com>
2023-03-14 23:44:43 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -40,8 +40,6 @@ export const ContactForm = () => {
           title="Name may contain only letters (less then 25), apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           maxlength="25"
           required
-          // value={name}
-          // onChange={handleChange}
         />
       </ContactLabel>
       <ContactLabel>
@@ -52,12 +50,9 @@ export const ContactForm = () => {
           placeholder="Enter contact phone number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          // value={number}
-          // onChange={handleChange}
           required
         />
       </ContactLabel>
-
       <ContactButton type="submit">Add contact</ContactButton>
     </ContactFormStyled>
   );
